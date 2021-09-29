aedc35d0a16876eaa6604f39ce99ee13fe61d44b
rfxct <xbrcvar@gmail.com>
2021-09-29 11:47:37 -0300

    Rewrite login command

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -1,17 +1,29 @@
 import { Command } from '@oclif/command'
-import Spotify from '../apis/Spotify'
+import cli from 'cli-ux'
+
+import Spotify from '../wrappers/Spotify'
+import Cache from '../wrappers/Cache'
 
 import * as nconf from 'nconf'
-
 nconf.file({ file: 'cache/auth.json' })
 
 export default class Login extends Command {
   static description = 'Conecta à sua conta do Spotify utilizando o OAuth2'
 
   async run() {
-    const result = await Spotify.login()
+    await cli.open([
+      'https://accounts.spotify.com/authorize?',
+      new URLSearchParams({
+        response_type: 'code',
+        client_id: (process.env.CLIENT_ID as string),
+        scope: (process.env.SCOPES as string).split(',').join(' '),
+        redirect_uri: (process.env.REDIRECT_URI as string)
+      })
+    ].join(''))
 
-    Object.entries({ generated_at: Date.now(), ...result }).forEach(values => nconf.set(...values))
-    nconf.save({})
+    const result = await Spotify.getToken()
+    Cache.persistAuth(result)
+    
+    console.log('Autenticado com sucesso')
   }
 }
